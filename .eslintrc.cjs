0cc1324628d8ecd219bd532ee28c5b6f354b6ca4
masamiki matsubara <masamiki.matsubara@amd-lab.com>
2022-02-11 18:51:07 +0900

    add lint settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+    'tsconfigRootDir': __dirname,
+    'project': ['./tsconfig.json'],
+    'extraFileExtensions': ['.svelte']
+  },
+  'plugins': [
+    'svelte3',
+    '@typescript-eslint',
+  ],
+  'overrides': [
+    {
+      'files': ['*.svelte'],
+      'processor': 'svelte3/svelte3'
+    }
+  ],
+  'settings': {
+    'svelte3/typescript': require('typescript'),
+    'svelte3/ignore-styles': () => true
+  },
+  'ignorePatterns': ['node_modules'],
+};
